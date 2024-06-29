6fccc06e301d4fecd6feaeea93d53e672591d3e8
Scofield <scofield.liu@mystenlabs.com>
2024-06-29 21:02:04 +0100

    nit: rm old mock_consensus

--- crates/remora/src/lib.rs
+++ crates/remora/src/lib.rs
@@ -2,7 +2,6 @@ pub mod agents;
 pub mod input_traffic_manager;
 pub mod metrics;
 pub mod mock_consensus;
-pub mod mock_consensus_worker;
 pub mod pre_exec_agent;
 pub mod pre_exec_worker;
 pub mod primary_agent;

f4e24a4e0e85273718542eaaeca6187d3f2db818
Scofield <scofield.liu@mystenlabs.com>
2024-08-17 15:47:06 +0100

    feat: warm-up phase in load-gen

--- crates/remora/src/load_generator.rs
+++ crates/remora/src/load_generator.rs
@@ -43,17 +43,20 @@ impl LoadGenerator {
         generate_transactions(&self.config).await
     }
 
-    /// Run the load generator. This will submit transactions to the system at the specified rate
-    /// until all transactions are submitted.
-    pub async fn run(&mut self, transactions: Vec<CertifiedTransaction>) {
-        let precision = if self.config.load > 1000 { 20 } else { 1 };
-        let burst_duration = Duration::from_millis(1000 / precision);
+    // Function to run the transaction submission at a specific load
+    async fn submit_transactions(
+        &mut self,
+        transactions: Vec<CertifiedTransaction>,
+        load: u64,
+        precision: u64,
+        burst_duration: Duration,
+    ) {
         let mut interval = interval(burst_duration);
         interval.set_missed_tick_behavior(MissedTickBehavior::Skip);
 
-        let chunks_size = (self.config.load / precision) as usize;
+        let chunks_size = (load / precision) as usize;
         let chunks = &transactions.into_iter().chunks(chunks_size);
-        tracing::info!("Submitting transactions...");
+
         for (counter, chunk) in chunks.into_iter().enumerate() {
             if counter % 1000 == 0 && counter != 0 {
                 tracing::debug!("Submitted {} txs", counter * chunks_size);
@@ -76,6 +79,77 @@ impl LoadGenerator {
 
             interval.tick().await;
         }
+    }
+
+    pub async fn run(&mut self, transactions: Vec<CertifiedTransaction>) {
+        let warm_up_load = 2_000;
+        let real_load = self.config.load;
+
+        // If the real load is less than or equal to the warm-up load, skip the warm-up
+        // used for test cases
+        if real_load <= warm_up_load {
+            tracing::info!(
+                "Skipping warm-up phase as real load ({}) <= warm-up load ({})",
+                real_load,
+                warm_up_load
+            );
+            self.real_run(transactions).await;
+        } else {
+            tracing::info!("Starting warm-up and real run phases...");
+            self.warm_up_and_real_run(transactions, warm_up_load).await;
+        }
+    }
+
+    async fn warm_up_and_real_run(
+        &mut self,
+        transactions: Vec<CertifiedTransaction>,
+        warm_up_load: u64,
+    ) {
+        let warm_up_duration = Duration::from_secs(1);
+
+        // Warm-up configuration
+        tracing::info!("Starting warm-up phase at {} load...", warm_up_load);
+        let warm_up_precision = if warm_up_load > 1_000 { 20 } else { 1 };
+        let warm_up_burst_duration = Duration::from_millis(1_000 / warm_up_precision);
+
+        // Calculate how many transactions are needed for the warm-up phase
+        let warm_up_chunk_size = (warm_up_load / warm_up_precision) as usize;
+        let warm_up_tx_count = warm_up_chunk_size
+            * (warm_up_duration.as_secs_f64() * warm_up_precision as f64) as usize;
+
+        tracing::info!(
+            "warm-up len {}, total_len {}",
+            warm_up_tx_count,
+            transactions.len(),
+        );
+
+        // Split the transactions into warm-up and real run transactions
+        let (warm_up_transactions, remaining_transactions) =
+            transactions.split_at(warm_up_tx_count);
+
+        let warm_up_future = self.submit_transactions(
+            warm_up_transactions.to_vec(), // Use the warm-up transactions
+            warm_up_load,
+            warm_up_precision,
+            warm_up_burst_duration,
+        );
+
+        // Use a timeout to limit the warm-up phase duration
+        let _ = tokio::time::timeout(warm_up_duration, warm_up_future).await;
+
+        // After warm-up, proceed to the real run
+        self.real_run(remaining_transactions.to_vec()).await;
+    }
+
+    async fn real_run(&mut self, transactions: Vec<CertifiedTransaction>) {
+        let real_load = self.config.load;
+        tracing::info!("Starting real run at {} load...", real_load);
+
+        let precision = if real_load > 1_000 { 20 } else { 1 };
+        let burst_duration = Duration::from_millis(1_000 / precision);
+
+        self.submit_transactions(transactions, real_load, precision, burst_duration)
+            .await;
     }
 }
 
